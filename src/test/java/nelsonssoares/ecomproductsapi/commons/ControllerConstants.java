e57c5764039983ef6da65ec07301b460a029ce8b
Nelson <punkpunkada@gmail.com>
2024-03-22 18:14:23 -0300

    starting categorycontroller test

--- src/test/java/nelsonssoares/ecomproductsapi/commons/ControllerConstants.java
+++ src/test/java/nelsonssoares/ecomproductsapi/commons/ControllerConstants.java
@@ -2,7 +2,12 @@ package nelsonssoares.ecomproductsapi.commons;
 
 public class ControllerConstants {
 
+    //PRODUTOS
     public static final String API_BASE_URL = "/ecommerce/produtos";
     public static final String ID = "/{id}";
     public static final String NAME = "/name/{nome}";
+
+
+    //CATEGORIA
+    public static final String CATEGORY ="/categoria";
 }
--- src/test/java/nelsonssoares/ecomproductsapi/entrypoints/CategoryControllerTest.java
+++ src/test/java/nelsonssoares/ecomproductsapi/entrypoints/CategoryControllerTest.java
@@ -2,10 +2,20 @@ package nelsonssoares.ecomproductsapi.entrypoints;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import nelsonssoares.ecomproductsapi.services.CategoryService;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static nelsonssoares.ecomproductsapi.commons.CategoryConstants.*;
+import static nelsonssoares.ecomproductsapi.commons.ControllerConstants.API_BASE_URL;
+import static nelsonssoares.ecomproductsapi.commons.ControllerConstants.CATEGORY;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(CategoryController.class)
 public class CategoryControllerTest {
@@ -17,4 +27,25 @@ public class CategoryControllerTest {
 
     @MockBean
     private CategoryService categoryService;
+
+    @Test
+    public void createCategory_withValidInput_shouldReturnCreated() throws Exception {
+
+        when(categoryService.save(VALID_CATEGORYDTO)).thenReturn(VALID_CATEGORY_RESPONSE);
+        mockMvc.perform(post(API_BASE_URL+CATEGORY)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(VALID_CATEGORYDTO)))
+                .andExpect(status().isCreated());
+
+    }
+
+    @Test
+    public void createCategory_withInvalidInput_shouldReturnBadRequest() throws Exception {
+
+        when(categoryService.save(INVALID_CATEGORYDTO)).thenReturn(VALID_CATEGORY_BADREQUEST);
+        mockMvc.perform(post(API_BASE_URL+CATEGORY)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(INVALID_CATEGORYDTO)))
+                .andExpect(status().isBadRequest());
+    }
 }
